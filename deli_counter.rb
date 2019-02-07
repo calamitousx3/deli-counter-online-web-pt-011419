fe9906dd4884f3dd33c4a0863f4f9ae945979ae3
Carmen Renee <carmengenpt@gmail.com>
2019-02-07 23:02:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ def now_serving(array)
   if array.length == 0 
     puts "There is nobody waiting to be served!"
   else 
-    
+    puts "Currently serving #{name}."
     array.shift
   end
 end 
